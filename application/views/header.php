41dbcfe107d8c7a8727b062d113ed014c4161998
Goose <goosepostbox@gmail.com>
2017-10-02 23:09:40 -0400

    Full 4chan style, remove bootstrap
    Don't need or want it this go round.

--- application/views/header.php
+++ application/views/header.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="icon" href="<?=base_url()?>resources/img/favicon.ico">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 
     <link href="<?=base_url()?>resources/chan/burichan.css" rel="stylesheet" type="text/css">
     <!-- Blue -->
--- application/views/main.php
+++ application/views/main.php
@@ -3,10 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-push-2">
-			<h1 class="text-center">GuessChan</h1>
+		<div class="stuff_container">
+			<h1>GuessChan</h1>
 			<hr>
-			<span class="the_post"><?php echo $random['post']; ?></span>
+
+			<div class="postContainer replyContainer" id="pc1234567899">
+				<div class="sideArrows" id="sa1234567899">&gt;&gt;</div>
+				<div id="p1234567899" class="post reply">
+					<div class="postInfoM mobile" id="pim1234567899"><span class="nameBlock"><span class="name">Anonymous</span><br></span><span class="dateTime postNum" data-utc="5678">04/20/69(Wed)16:20:42 <a href="#" title="Link to this post">No.</a><a href="#" title="Reply to this post">1234567899</a></span></div>
+					<div class="postInfo desktop" id="pi1234567899"><input type="checkbox" name="1234567899" value="delete"> <span class="nameBlock"><span class="name">Anonymous</span> </span> <span class="dateTime" data-utc="5678">04/20/69(Wed)16:20:42</span> <span class="postNum desktop"><a href="#" title="Link to this post">No.</a><a href="#" title="Reply to this post">1234567899</a></span></div>
+					<blockquote class="postMessage greentext_this embedica_this" id="m1234567899">
+						<!-- Here is the post -->
+						<span class="the_post"><?php echo $random['post']; ?></span>
+					</blockquote>
+			   </div>
+			</div>
+
 			<span class="answer" style="display: none"><?php echo $random['board'] ?></span>
 			<br>
 			<hr>
