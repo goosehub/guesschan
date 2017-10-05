81e01619147cf1741ae8e3fe7be86eb254e121b1
Goose <goosepostbox@gmail.com>
2017-10-04 22:04:31 -0400

    UI tweaks, mobile, next button
    Next button makes the game much more screencapable.

--- application/views/main.php
+++ application/views/main.php
@@ -4,20 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="main_container">
 	<header>
 		<div class="header_compartment header_left">
-			<h1 class="main_header_text">GuessChan</h1>
+			<h1 class="main_header_text">GuessChan?</h1>
 		</div>
 		<div class="header_compartment header_center">
-			<div class="last_answer_parent">
-				<span class="last_answer_status">N/A</span>
-				<span class="last_answer_board"></span>
-				<a class="last_answer_link" target="_blank" href="#">View on 4chan</a>
-			</div>
 		</div>
 		<div class="header_compartment header_right">
 			<div class="score_parent">
-				<span class="right_parent"><span class="right_number">0</span></span>
-				<span class="wrong_parent"><span class="wrong_number">0</span></span>
-				<span class="total_parent"><span class="total_number">0</span></span>
+				<span class="right_parent">Right: <span class="right_number">0</span></span>
+				<span class="wrong_parent">Wrong: <span class="wrong_number">0</span></span>
+				<!-- <span class="total_parent">Total: <span class="total_number">0</span></span> -->
 			</div>
 		</div>
 	</header>
@@ -36,25 +31,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	   </div>
 	</div>
 
+	<div class="last_answer_parent">
+		<span class="last_answer_element last_answer_status">N/A</span>
+		<span class="last_answer_element that_was">That was </span>
+		<span class="last_answer_element last_answer_board"></span>
+		<a class="last_answer_element last_answer_link" target="_blank" href="#">View on 4chan</a>
+		<button class="next_button">Next</button>
+	</div>
+
 	<span class="answer_board_abbr" style="display: none"><?php echo $random['board_abbr'] ?></span>
 	<span class="answer_board_title" style="display: none"><?php echo $random['board_title'] ?></span>
 	<span class="answer_link" style="display: none"><?php echo $random['link'] ?></span>
 	<br>
 	<hr>
-	<p class="lead">Select board you think this post came from</p>
-	<?php $i = 0; ?>
-	<?php foreach ($boards as $board) { ?>
-		<a href="#" class="board_selection" board="<?php echo $board['board']; ?>">/<?php echo $board['board']; ?>/ - <?php echo $board['title']; ?></a>
-		<br>
-		<?php $i++; ?>
-	<?php } ?>
+	<div class="selection_compartment selection_left">
+		<p><span class="or_mobile">Or</span> Select the board you think this post came from</p>
+		<?php $i = 0; ?>
+		<?php foreach ($boards as $board) { ?>
+			<a href="#" class="board_selection" board="<?php echo $board['board']; ?>">/<?php echo $board['board']; ?>/ - <?php echo $board['title']; ?></a>
+			<br>
+			<?php $i++; ?>
+		<?php } ?>
+	</div>
+	<div class="selection_compartment selection_right">
+		<p><span class="or_desktop">Or</span> Enter the board abbreviation here</p>
+		<input id="board_text_input" type="text"/>
+		<button id="board_text_submit" type="submit">Submit</button>
+	</div>
 
 	<br>
 	<br>
 	<hr>
 	<div class="how_it_works">
 		<strong>How it works</strong>
-		<p class="quote">>A random post from a random page of a random board is selected. All boards are choosen with equal likelyhood, meaning /b/ is not more likely than /n/. Post number quotes (>>1234) are removed because they are too big a clue. There's no ads or tracking, not even Google Analytics. This was built proudly with PHP using CodeIgniter 3 and the 4chan API. [s4s] is nice board.</p>
+		<p class="quote">>A random post from a random page of a random board is selected. All boards are choosen with equal likelyhood, meaning /b/ is as likely to pop up as /n/. Most post numbers (>>1234) are removed because they are too big a clue. This was built proudly with PHP using CodeIgniter 3 and the 4chan API. You can view and contribute to the code on <a target="_blank" href="https://github.com/goosehub/guesschan">GitHub</a>. [s4s] is nice board.</p>
 	</div>
 	<br>
 </div>
--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
-	console.log('No need to cheat with inspect element. Just click the post number to view the post');
+	console.log('No need to cheat with inspect element. Just click the post number to view the post. Also, check out the GitHub.');
+
+	$('#board_text_submit').click(function(){
+		answer_submitted($('#board_text_input').val());
+	});
+
+	$('#board_text_input').on('keyup', function(e){
+	    if (e.keyCode == 13) {
+			answer_submitted($('#board_text_input').val());
+	    }
+	});
+
 	$('.board_selection').click(function(){
+		answer_submitted($(this).attr('board'));
+	});
+
+	$('.next_button').click(function(){
+		get_next();
+	});
+
+	function answer_submitted(answer) {
 		$('.last_answer_parent').show();
 		$('.last_answer_board').html('/' + $('.answer_board_abbr').html() + '/' + ' - ' + $('.answer_board_title').html());
 		$('.last_answer_link').attr('href', $('.answer_link').html());
-		if ($('.answer_board_abbr').html() === $(this).attr('board')) {
+		$('#board_text_input').val('');
+
+		// Easter Egg
+		if (answer === '[s4s]') {
+			alert('[s4s] is nice board');
+		}
+
+		if ($('.answer_board_abbr').html() === answer) {
 			$('.last_answer_status').html('Right!');
 			$('.last_answer_status').removeClass('wrong_status').addClass('right_status');
 			$('.right_number').html(parseInt($('.right_number').html()) + 1);
@@ -14,8 +40,10 @@ $(document).ready(function(){
 			$('.last_answer_status').removeClass('right_status').addClass('wrong_status');
 			$('.wrong_number').html(parseInt($('.wrong_number').html()) + 1);
 		}
-
 		$('.total_number').html(parseInt($('.total_number').html()) + 1);
+	}
+
+	function get_next() {
 		full_url = 'main/get_random_4chan_post';
 		console.log('Get next post');
 		$.getJSON(full_url, function(data) {
@@ -25,6 +53,7 @@ $(document).ready(function(){
 			$('.answer_link').html(data.link);
 			$('.answer_board_abbr').html(data.board_abbr);
 			$('.answer_board_title').html(data.board_title);
+			$('.last_answer_parent').hide();
 		});
-	});
+	}
 });
